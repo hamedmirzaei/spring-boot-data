4351b0a5aab61b6474aa03e5f0e83bd6a9e62452
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-05-04 15:19:12 +0430

    custom query example is added

--- src/main/java/spring/boot/data/SpringBootDataApplication.java
+++ src/main/java/spring/boot/data/SpringBootDataApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 import spring.boot.data.model.Customer;
 import spring.boot.data.repository.CustomerRepository;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @Slf4j
@@ -20,6 +23,7 @@ public class SpringBootDataApplication implements CommandLineRunner {
         SpringApplication.run(SpringBootDataApplication.class, args);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public void run(String... args) throws Exception {
         // save a couple of customers
@@ -49,6 +53,13 @@ public class SpringBootDataApplication implements CommandLineRunner {
         customerRepository.findByLastName("Mirzaei 2").forEach(mirzaeiCustomer -> {
             log.info(mirzaeiCustomer.toString());
         });
+        log.info("");
+
+        // for stream, need @Transactional
+        log.info("Customer found with findByFirstNameReturnStream(@Param(\"firstName\") String firstName):");
+        try (Stream<Customer> stream = customerRepository.findByFirstNameReturnStream("Hamed")) {
+            stream.forEach(x -> log.info(x.toString()));
+        }
         log.info("");
     }
 }
--- src/main/java/spring/boot/data/repository/CustomerRepository.java
+++ src/main/java/spring/boot/data/repository/CustomerRepository.java
@@ -1,12 +1,18 @@
 package spring.boot.data.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import spring.boot.data.model.Customer;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     List<Customer> findByLastName(String lastName);
+
+    @Query("select c from Customer c where c.firstName = :firstName")
+    Stream<Customer> findByFirstNameReturnStream(@Param("firstName") String firstName);
 
 }
